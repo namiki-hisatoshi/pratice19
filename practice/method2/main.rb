543f7340df6aefd7630fd186ca1b816a5113599d
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2020-12-24 23:08:56 +0900

    practice11

--- practice/method2/main.rb
+++ practice/method2/main.rb
@@ -1,18 +1,19 @@
 require './method'
 
-
 plans = [
-  {place: "沖縄", price: 10000},
-  {place: "北海道", price: 20000},
-  {place: "九州", price: 15000}
+  {place: "沖縄", price:10000},
+  {place: "北海道", price:20000},
+  {place: "九州", price:15000}
 ]
 
+
 # 目次
-# プランを表示する
+
+# プランを表示
 disp_plan(plans)
-# プランを選択する
+# プランを選択
 chosen_plan = choose_plan(plans)
-# 人数を選択する
-quantity_of_plan = reservation(chosen_plan)
-# 合計金額を表示する
-calculate_chages(chosen_plan, quantity_of_plan)
+# 人数を選択
+quantity_of_num = reservation(chosen_plan)
+# 合計金額を表示
+calculate_money(chosen_plan, quantity_of_num)
--- practice/method2/method.rb
+++ practice/method2/method.rb
@@ -1,38 +1,39 @@
 # 本編
-# プランを表示する
+
+# プランを表示
 def disp_plan(plans)
   puts "旅行プランを選択して下さい。"
   plans.each.with_index(1) do |plan, i|
-      puts "#{i}. #{plan[:place]}旅行 (#{plan[:price]}円)"
+    puts "#{i}. #{plan[:place]}旅行 (#{plan[:price]}円)"
   end
 end
-# プランを選択する
+# プランを選択
 def choose_plan(plans)
-  print "プランの番号を選択 > "
   while true
-    select_plan_num = gets.to_i
-    break if(1..3).include?(select_plan_num)
-      puts "1~3番で選択して下さい"
-    end
-  plans[select_plan_num - 1]
+  print "プランの番号を選択 > "
+    select_num = gets.to_i
+    break if(1..3).include?(select_num)
+    puts "1~3の番号で選んでください。"
+  end
+  plans[select_num - 1]
 end
-# 人数を選択する
+# 人数を選択
 def reservation(chosen_plan)
   puts "#{chosen_plan[:place]}旅行ですね。何名で予約されますか？"
-  print "人数を入力　> "
   while true
-  quantity_of_plan = gets.to_i
-  break if quantity_of_plan.to_i >= 1
-   puts "１以上を入力して下さい。"
- end
- quantity_of_plan
+    puts "人数を入力 > "
+    quantity_of_num = gets.to_i
+    break if(quantity_of_num.to_i >= 1)
+    puts "1以上を入力して下さい。"
+  end
+  quantity_of_num
 end
-# 合計金額を表示する
-def calculate_chages(chosen_plan, quantity_of_plan)
-  puts "#{quantity_of_plan}名ですね。"
-  total_price = chosen_plan[:price] * quantity_of_plan
-  if quantity_of_plan >= 5
-    puts "5名以上ですので10%割引となります。"
+# 合計金額を表示
+def calculate_money(chosen_plan, quantity_of_num)
+  puts "#{quantity_of_num}名ですね。"
+  total_price = quantity_of_num * chosen_plan[:price]
+  if quantity_of_num >= 5
+    puts "5名以上ですので10%割引となります"
     total_price *= 0.9
   end
   puts "合計金額は#{total_price.floor}円になります。"
